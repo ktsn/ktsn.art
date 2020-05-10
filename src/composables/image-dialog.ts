5bb7e8e0ba725d5a03b3bad38d73f81e03f99695
ktsn <ktsn55@gmail.com>
2020-05-10 22:14:57 +0800

    allow disposing illust dialog by dragging it

---
+++ src/composables/image-dialog.ts
@@ -0,0 +1,84 @@
+import { ref, computed } from 'vue'
+
+export function useImageDialog(exitDialog: () => void) {
+  const dragThreshold = 170
+
+  const dragging = ref(false)
+  const dragStartY = ref(0)
+  const dragY = ref(0)
+  const cancelling = ref(false)
+
+  const deltaY = computed(() => dragY.value - dragStartY.value)
+
+  const backgroundOpacity = computed(() => {
+    const progress = Math.min(Math.abs(deltaY.value) / dragThreshold)
+    return (1 - progress) * 0.9 + 0.1 // Map the value from 0.1 to 1
+  })
+
+  const backgroundClass = computed(() => {
+    if (!cancelling.value) {
+      return
+    }
+    return 'transition duration-200 ease-out'
+  })
+
+  const imageClass = computed(() => {
+    if (!cancelling.value) {
+      return
+    }
+    return 'transition duration-200 ease-out'
+  })
+
+  const imageStyle = computed(() => {
+    return {
+      transform: `translateY(${deltaY.value}px)`,
+    }
+  })
+
+  function startDrag(event: PointerEvent) {
+    dragging.value = true
+    dragStartY.value = dragY.value = event.pageY
+  }
+
+  function continueDrag(event: PointerEvent) {
+    if (dragging.value) {
+      dragY.value = event.pageY
+    }
+  }
+
+  function endDrag() {
+    if (!dragging.value) {
+      return
+    }
+
+    if (deltaY.value === 0) {
+      dragging.value = false
+      return
+    }
+
+    if (Math.abs(deltaY.value) < dragThreshold) {
+      cancelDrag()
+    } else {
+      exitDialog()
+    }
+    dragging.value = false
+  }
+
+  function cancelDrag() {
+    if (dragging.value) {
+      dragging.value = false
+      cancelling.value = true
+      dragY.value = dragStartY.value = 0
+    }
+  }
+
+  return {
+    backgroundOpacity,
+    backgroundClass,
+    imageClass,
+    imageStyle,
+    startDrag,
+    continueDrag,
+    endDrag,
+  }
+}
--- src/pages/Illust.vue
+++ src/pages/Illust.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed inset-0" @mousemove="onHover" @touchstart="onHover">
+  <div class="fixed inset-0">
     <transition
       appear
       enter-active-class="transition ease-out duration-200"
@@ -7,7 +7,13 @@
       leave-active-class="transition ease-out duration-200"
       leave-to-class="opacity-0"
     >
-      <div v-if="!leaving" class="absolute inset-0 bg-gray-900" />
+      <div v-if="!leaving">
+        <div
+          class="absolute inset-0 bg-gray-900"
+          :class="dialog.backgroundClass"
+          :style="{ opacity: dialog.backgroundOpacity }"
+        />
+      </div>
     </transition>
 
     <transition
@@ -17,62 +23,46 @@
       leave-active-class="transition duration-200 ease-out"
       leave-to-class="opacity-0 transform scale-75"
     >
-      <div v-if="illust.result && !leaving" class="absolute inset-0">
-        <transition
-          leave-active-class="transition-all duration-500"
-          leave-to-class="blur-0"
-        >
-          <img
-            v-if="!imageLoaded"
-            class="absolute inset-0 w-full h-full object-contain blur"
-            :src="illust.result.thumbnailUrl"
-            alt=""
-          />
-        </transition>
-
-        <transition
-          enter-active-class="transition duration-500 ease-out"
-          enter-from-class="opacity-0"
-        >
-          <img
-            v-show="imageLoaded"
-            class="absolute inset-0 w-full h-full object-contain"
-            :src="illust.result.displayUrl"
-            alt=""
-            @load="imageLoaded = true"
-          />
-        </transition>
-      </div>
-    </transition>
-
-    <transition
-      enter-active-class="transition duration-200 ease-out"
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-      leave-active-class="transition duration-200 ease-out"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
-      <div
-        v-if="!leaving"
-        v-show="showMenu"
-        class="absolute inset-x-0 top-0 bg-black bg-opacity-75 px-5 py-3"
+      <router-link
+        v-if="illust.result && !leaving"
+        class="absolute inset-0"
+        to="/"
+        @pointerdown.prevent="dialog.startDrag"
+        @pointermove.prevent="dialog.continueDrag"
+        @pointerup.prevent="dialog.endDrag"
+        @pointercancel.prevent="dialog.endDrag"
       >
-        <VButtonIcon to="/">
-          <svg
-            title="Close"
-            fill="#ffffff"
-            height="32"
-            viewBox="0 0 24 24"
-            width="32"
+        <div
+          class="absolute inset-0"
+          :class="dialog.imageClass"
+          :style="dialog.imageStyle"
+        >
+          <transition
+            leave-active-class="transition-all duration-500"
+            leave-to-class="blur-0"
           >
-            <path
-              d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+            <img
+              v-if="!imageLoaded"
+              class="absolute inset-0 w-full h-full object-contain blur"
+              :src="illust.result.thumbnailUrl"
+              alt=""
             />
-            <path d="M0 0h24v24H0z" fill="none" />
-          </svg>
-        </VButtonIcon>
-      </div>
+          </transition>
+
+          <transition
+            enter-active-class="transition duration-500 ease-out"
+            enter-from-class="opacity-0"
+          >
+            <img
+              v-show="imageLoaded"
+              class="absolute inset-0 w-full h-full object-contain"
+              :src="illust.result.displayUrl"
+              alt=""
+              @load="imageLoaded = true"
+            />
+          </transition>
+        </div>
+      </router-link>
     </transition>
   </div>
 </template>
@@ -80,28 +70,22 @@
 <script lang="ts">
 import { Vue, Options, setup } from 'vue-class-component'
 import { toRef } from 'vue'
+import { useRouter } from 'vue-router'
 import VButtonIcon from '../components/VButtonIcon.vue'
 import { useIllust } from '../composables/illusts'
+import { useImageDialog } from '../composables/image-dialog'
 
 class Illust extends Vue {
   illustKey!: string
 
   illust = setup(() => useIllust(toRef(this, 'illustKey')))
 
-  showMenuTimer: any = undefined
-  showMenu: boolean = false
-
+  dialog = setup(() => {
+    const router = useRouter()
+    return useImageDialog(() => router.push('/'))
+  })
   imageLoaded: boolean = false
   leaving: boolean = false
-
-  onHover() {
-    clearTimeout(this.showMenuTimer)
-    this.showMenu = true
-
-    this.showMenuTimer = setTimeout(() => {
-      this.showMenu = false
-    }, 2000)
-  }
 
   beforeRouteLeave(_to: unknown, _from: unknown, next: () => void) {
     this.leaving = true
